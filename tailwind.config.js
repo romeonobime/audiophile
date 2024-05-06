919dd0c6742f94e3fa808e8471e740a93ba9e550
Romeo Nobime <romeo.nobime@protonmail.com>
2024-05-06 23:13:28 +0200

    added colors and screens for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
+    colors: {
+      'primary': '#D87D4A',
+      'primary-light': '#fbaf85',
+      'danger': '#CD2C2C',
+      'neutral-lightest': '#ffffff',
+      'neutral-lighter': '#FAFAFA',
+      'neutral-light': '#F1F1F1',
+      'neutral-darker': '#101010',
+      'neutral-darkest': '#000000',
+    },
+    screens: {
+      'sm': '641px',
+      'md': '769px',
+    },
     extend: {},
   },
   plugins: [],
